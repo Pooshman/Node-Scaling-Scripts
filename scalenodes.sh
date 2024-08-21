eb40d8b5135e70cafe99bd6c3817dad7d6e5fda0
Purush Ram <purushnram@gmail.com>
2024-08-21 11:23:19 -0700

    Update scalenodes.sh

--- scalenodes.sh
+++ scalenodes.sh
@@ -4,20 +4,22 @@
 CLUSTER_NAME="my-cluster-3"
 QUEUE_NAME="queue-1"  # The name of your Slurm queue
 COMPUTE_RESOURCE="queue-1-cr-1"  # The name of your compute resource within the queue
-MIN_NODES=0  # Minimum number of nodes (can be 0 to allow scaling down to zero)
+MIN_NODES=0  # Minimum number of nodes
 MAX_NODES=10  # Maximum number of nodes
 DESIRED_NODES=5  # The desired number of nodes you want to scale to
 REGION="us-east-1"  # Specify your AWS region here
 
 # Function to scale up the nodes
 scaleUp() {
-  aws ec2 modify-instance-attribute \
-    --instance-id $(aws ec2 describe-instances --filters "Name=tag:aws:parallelcluster:cluster-name,Values=$CLUSTER_NAME" "Name=tag:aws:parallelcluster:queue-name,Values=$QUEUE_NAME" "Name=tag:aws:parallelcluster:compute-resource-name,Values=$COMPUTE_RESOURCE" --query "Reservations[*].Instances[*].InstanceId" --output text --region $REGION) \
+  aws parallelcluster update-compute-fleet \
+    --cluster-name $CLUSTER_NAME \
     --region $REGION \
-    --instance-type $COMPUTE_RESOURCE \
-    --min-count $MIN_NODES \
-    --max-count $MAX_NODES \
-    --desired-count $DESIRED_NODES
+    --status RUNNING
+
+  aws parallelcluster update-compute-fleet \
+    --cluster-name $CLUSTER_NAME \
+    --region $REGION \
+    --compute-resources "[{\"Name\":\"$COMPUTE_RESOURCE\",\"MinCount\":$MIN_NODES,\"MaxCount\":$MAX_NODES,\"DesiredCount\":$DESIRED_NODES}]"
 }
 
 # Call the function
@@ -29,3 +31,4 @@ if [ $? -eq 0 ]; then
 else
   echo "Failed to scale the compute nodes."
 fi
+
