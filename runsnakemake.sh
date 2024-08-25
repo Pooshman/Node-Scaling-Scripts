f7c46c2ef90494f88b3b71b713012ef0f37fc7c0
Purush Ram <purushnram@gmail.com>
2024-08-25 10:42:50 -0700

    Update runsnakemake.sh

--- runsnakemake.sh
+++ runsnakemake.sh
@@ -2,12 +2,12 @@
 #SBATCH --job-name=snakemake_job
 #SBATCH --nodes=5
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
-#SBATCH --time=01:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --time=00:10:00
 #SBATCH --output=snakemake_output_%j.txt
 
 # Activate your Snakemake environment
-#conda activate snakemake
+conda activate snakemake
 
 # Change to the directory where the Snakefile is located
 #cd (Snakefile directory)
@@ -15,5 +15,6 @@
 #NOTE: The activate and cd commands could be implemented downline for streamlining (auto commands). 
 #Also add miniforge3 miniconda3 installation commands
 
-# Run Snakemake with the SLURM profile
-snakemake --snakefile Snakefile --jobs 100 --profile config/slurm
+# Run Snakemake
+snakemake --snakefile Snakefile --jobs 5 --profile config/slurm
+
