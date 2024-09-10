600496cb122169c89ec17ca9b6b393d56ff23ff0
Purush Ram <purushnram@gmail.com>
2024-09-09 22:12:22 -0700

    Update test_snakemake_cmd.sh

--- test_snakemake_cmd.sh
+++ test_snakemake_cmd.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-# Set variables
+# Activate Snakemake environment
+conda activate snakemake
+
+# Define variables
 cores=16
 mode="fast"
 config_path="config/config.yaml"
 num_threads=16
 
-# Snakemake command to test
-snakemake \
-    --cores $cores \
-    --config mode=$mode config_path=$config_path num_threads=$num_threads \
-    --use-conda \
-    --rerun-incomplete \
-    --cluster-config cluster.yaml \
-    --jobs $cores \
-    --keep-going \
-    --latency-wait 60 \
-    --cluster "sbatch -A your-account -p queue-1 --nodes=1 --ntasks=${num_threads} --time=24:00:00"
+# Snakemake command
+snakemake_cmd=(
+    "snakemake"
+    "--cores" "$cores"
+    "--config" "mode=$mode" "config_path=$config_path" "num_threads=$num_threads"
+    "--use-conda"
+    "--rerun-incomplete"
+    "--jobs" "$cores"  # Maximum jobs to submit at once
+    "--keep-going"
+    "--latency-wait" "60"
+    "--cluster" "sbatch -A your-account -p queue-1 --nodes=1 --ntasks={threads} --time=24:00:00"
+)
+
+# Run Snakemake command
+"${snakemake_cmd[@]}"
+
